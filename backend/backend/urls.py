70e6daac3752a2ee0dd1fe75cd3c6111dd6c430e
daniel.jeschor <daniel.jeschor@mailbox.tu-dresden.de>
2022-08-10 15:58:37 +0200

    Change url of admin panel

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('news/', include('news.urls')),
-    path('admin/', admin.site.urls),
+    path('news/admin/', admin.site.urls),
 ]
