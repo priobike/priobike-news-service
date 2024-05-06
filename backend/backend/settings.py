e78b0de355594b255939ac3df7938ca02acd6a3d
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-05-06 10:07:30 +0200

    Use sqlite during tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,6 +125,12 @@ DATABASES = {
     }
 }
 
+if TESTING:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
